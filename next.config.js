d839cda16121040236457520286315b7fbb3e55b
Dave Manuel <jdmanuel242@gmail.com>
2022-11-24 20:54:25 +0800

    chore: update MongoDB

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
 module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       env: {
-        mongodb_username: 'maximilian',
-        mongodb_password: '2YkcXq43KyPk0vqp',
-        mongodb_clustername: 'cluster0',
-        mongodb_database: 'my-site-dev',
+        mongodb_username: "dave",
+        mongodb_password: "xnKgJLRtv7YKOO66",
+        mongodb_clustername: "cluster0",
+        mongodb_database: "udemy-next-js-dev",
       },
     };
   }
 
   return {
     env: {
-      mongodb_username: 'maximilian',
-      mongodb_password: '2YkcXq43KyPk0vqp',
-      mongodb_clustername: 'cluster0',
-      mongodb_database: 'my-site',
+      mongodb_username: "dave",
+      mongodb_password: "xnKgJLRtv7YKOO66",
+      mongodb_clustername: "cluster0",
+      mongodb_database: "udemy-next-js",
     },
   };
 };
